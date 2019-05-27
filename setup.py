d9ce78955fd1ac16baebc59b747bc1f67b832d67
Thomas Waters <twaters117@gmail.com>
2019-05-27 17:02:30 -0400

    Python 2.6 is no longer supported

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         ('License :: OSI Approved :: '
          'GNU Lesser General Public License v2 or later (LGPLv2+)'),
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
